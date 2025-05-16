020fdec38dc107b599e881d11896e3b338a15929
hjb0304 <shany34@naver.com>
2025-05-16 15:07:09 +0900

    ⚰️ cleanup: 룰렛 관련 코드 통합, 메뉴 리스트 배열 순서 변경

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './styles/global.css';
-import { spinRoulette } from './components/RouletteWheel';
-import { menuButtons } from './components/buttonManager';
 
 // DOM 생성
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -9,35 +7,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <canvas id="roulette-canvas" width="500" height="500"></canvas>
   </div>
 `;
-
-const canvas = document.querySelector('#roulette-canvas') as HTMLCanvasElement;
-
-const spinButton = document.querySelector('#spin') as HTMLButtonElement;
-let currentMenu: string[] = []; // 음식 리스트 저장소
-menuButtons();
-
-export function setCurrentMenu(menu: string[]) {
-  currentMenu = menu;
-}
-
-spinButton?.addEventListener('click', () => {
-  // 메뉴가 2개 미만이거나 모두 비어있을 경우
-  if (currentMenu.length < 2 || currentMenu.every(item => item.trim() === '')) {
-    alert('메뉴를 입력해주세요.');
-    return;
-  }
-  // currentMenu에 하나라도 빈 칸이 있는 경우
-  if (currentMenu.some(item => item.trim() === '')) {
-    alert('메뉴를 모두 입력해주세요.');
-    return;
-  }
-
-  const selectedIndex = spinRoulette(canvas, currentMenu);
-  spinButton.disabled = true;
-
-  canvas.addEventListener('transitionend', function handler() {
-    alert(currentMenu[selectedIndex]);
-    canvas.removeEventListener('transitionend', handler);
-    spinButton.disabled = false;
-  });
-});
