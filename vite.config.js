b7b6f6535a62e439a004ebaaf96955074259074c
hjb0304 <shany34@naver.com>
2025-05-21 15:17:00 +0900

    🔧 config: vite 페이지 index 페이지 외 제거

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: 'index.html', // 기본 index.html
-        list: 'src/pages/modal.html', // 추가 HTML 파일
-        // regist: 'src/pages/todo/regist.html', // 추가 HTML 파일
-        // info: 'src/pages/todo/info.html', // 추가 HTML 파일
-        // 필요한 다른 HTML 파일을 여기에 추가
       },
     },
   },
